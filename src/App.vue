fe9073befa64879825f0a7efd3c1ad0ba1817e91
Righthand5 <xyx1530966653@163.com>
2020-10-27 12:16:28 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <PostList></PostList>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import PostList from "./components/PostList";
+export default {
+  name: 'App',
+  components:{
+    Header,PostList
+  }
+}
+</script>
+
+<style>
+
+</style>
